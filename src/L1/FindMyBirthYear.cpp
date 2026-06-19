2f326a7df58b4897bf1f7fe499791d7ee4d6b063
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-06-19 16:45:38 +0300

    new edit

---
+++ src/L1/FindMyBirthYear.cpp
@@ -0,0 +1,15 @@
+//
+// Created by ali_h on 6/19/2026.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myAge;
+    cout << "Enter Your age: " << endl;
+    cin >> myAge;
+    int birthYear = 2026 - myAge;
+    cout << "Your Birth Year is: " << birthYear << endl;
+    return 0;
+}
