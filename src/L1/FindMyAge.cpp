229640fbb57064c2ea0fc380d596d9d5d5626b83
Ali H. <26713386+Ali-Haider8@users.noreply.github.com>
2026-06-19 16:38:16 +0300

    new codes

---
+++ src/L1/FindMyAge.cpp
@@ -0,0 +1,14 @@
+//
+// Created by ali_h on 6/19/2026.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int birthYear;
+    cout << "Enter Your Birth Year:  ";
+    cin >> birthYear;
+    int age = 2026 - birthYear;
+    cout << "Your age is: " << age;
+}
---
+++ src/L1/welcome.cpp
@@ -0,0 +1,10 @@
+//
+// Created by ali_h on 6/19/2026.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello World";
+
+    return 0;
+}
